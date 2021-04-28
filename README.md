4d1cb2aca96030c0432b5b63abc90178e5d62421
17588709050 <a7719665@163.com>
2021-04-28 16:55:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. cnpm i
+2. 将 `pre-commit` 文件放入 `.git\hooks` 目录中
+
---
+++ pages/main/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="test">
+    test
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'test',
+
+}
+</script>
+
+<style lang="scss" scoped>
+.test {
+
+}
+</style>
